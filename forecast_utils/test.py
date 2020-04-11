ee8a09e7b8dfacab3e3ee51430ee75569d08da32
Fernan <doskoy93@gmail.com>
2020-04-11 04:08:31 +0200

    Añadiendo ficheros

---
+++ forecast_utils/test.py
@@ -0,0 +1,14 @@
+import pytest
+import requests
+
+## Se prueba la conexión a la api 2 mediante una solicitud y comprobando el codigo de estado.
+def test_api2():
+	url = 'http://localhost:8082/v2/24'
+	solicitud = requests.get(url)
+	assert solicitud.status_code == 200
+
+## De forma similar al test anterior se prueba la conexion a la api 1.
+def test_api1():
+	url = 'http://localhost:8081/arima/24'
+	solicitud = requests.get(url)
+	assert solicitud.status_code == 200
